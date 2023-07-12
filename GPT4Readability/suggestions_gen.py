9ca5412405750e9549def15a48499cdc8bda09df
loevlie <loevliedenny@gmail.com>
2023-07-12 08:57:46 -0400

    Reducing the logging for suggestions

--- GPT4Readability/suggestions_gen.py
+++ GPT4Readability/suggestions_gen.py
@@ -2,6 +2,7 @@ import os
 from getpass import getpass
 from GPT4Readability.utils import *
 from tqdm import tqdm 
+import logging
 
 def generate_suggestions(root_dir, output_name, model):
     """Generates a suggestions.md file with suggested improvements to the code based on the python files in the provided directory
@@ -24,15 +25,17 @@ def generate_suggestions(root_dir, output_name, model):
     texts = split_docs(docs)
     chain = loadLLM(model)
     LOCAL_vector_store = makeEmbeddings(texts)
-    for doc in tqdm(python_files, desc="Optimizing the code"):
-        for func in tqdm(get_functions(doc),desc="Optimizing functions",leave=False):
+    print("Vector Database Built\n")
+    for doc in tqdm(python_files, desc="Optimizing files"):
+        for func in tqdm(get_functions(doc),desc=f"Optimizing functions in {doc}", leave=False):
             LOCAL_resp = askQs(LOCAL_vector_store, chain, inb_msg.replace("[INSERT FUNCTION HERE]",func['code']))
 
             if "NO CHANGES NEEDED" in LOCAL_resp:
                 continue 
             else:
                 out = f"\nfilepath: {func['filepath']}\n\nfunction_name: {func['function_name']}\n\noriginal function: \n```python\n{func['code']}\n```\n\nsuggestion: \n{LOCAL_resp}"
-                print(out)
+                # print(f"[INFO] Optimization suggestion generated for function {func['function_name']} in {func['filepath']}")
+                # print(out)
                 suggestions.append(out)
 
 
